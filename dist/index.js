1516fbff95bc8d61cae8f0def97febd783d14e6e
bluehexagons <bluehexagons@users.noreply.github.com>
2019-11-17 15:37:40 -0800

    Initial commit after moving to separate repo

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const Trace = require("./trace");
+console.log(`1 + 10 = ${Trace.runTrace('1 + 10')}`);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+// TODO: interactive console
+import * as Trace from './trace'
+
+console.log(`1 + 10 = ${Trace.runTrace('1 + 10')}`)
